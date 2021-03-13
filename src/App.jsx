445d8ff86fd7853506a90d5242fdc6b489fc2c7f
KunYuChang <mischiefsub@gmail.com>
2021-03-13 08:10:08 +0800

    transform state to game history

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,53 @@
+import { useState } from "react";
 import Board from "./components/Board";
+import { calculateWinner } from "./helpers";
 
 function App() {
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXNext: true },
+  ]);
+
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const current = history[currentMove];
+
+  console.log("history", history);
+
+  const winner = calculateWinner(current.board);
+  console.log(winner.winner);
+
+  const message = winner.winner
+    ? `Winnder is ${winner.winner}`
+    : `Next player is ${current.isXNext ? "X" : "O"}`;
+
+  const handleSqareClick = (position) => {
+    if (current.board[position] || winner.winner) {
+      return;
+    }
+
+    setHistory((prev) => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((square, pos) => {
+        if (pos === position) {
+          return last.isXNext ? "X" : "O";
+        }
+        return square;
+      });
+
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
+    });
+
+    setCurrentMove((prev) => prev + 1);
+  };
+
   return (
     <div className="app">
       <h1>Tic-Tac-Toe</h1>
-      <Board />
+      <h2>{message}</h2>
+      <Board board={current.board} handleSqareClick={handleSqareClick} />
     </div>
   );
 }
-
 
 export default App;
